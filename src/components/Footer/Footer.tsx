f5b6fd050d15151137414e43268752d48dcf057d
sheritsh <sheritsh@student.21-school.ru>
2023-08-16 12:15:13 +0300

    feat: make footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import classes from './Footer.module.css';
 import Container from '../UI/container/Container';
+import rs_logo from '';
 
 const Footer: React.FC = () => {
-  const footerContext = '<footer></footer>';
+  const authorText = 'created by:';
   return (
     <footer>
       <Container>
-        <div className={classes.footer}>{footerContext}</div>
+        <div className={classes.footer}>
+          <div className={classes.authors}>
+            {authorText} <br />
+            <ul>
+              <li>
+                <a href="https://github.com/ekatrif">ekatrif</a>
+              </li>
+              <li>
+                <a href="https://github.com/sheritsh">sheritsh</a>
+              </li>
+              <li>
+                <a href="https://github.com/montek1o">montek1o</a>
+              </li>
+            </ul>
+          </div>
+          <div className={classes.copyright}>Copyright ©2023</div>
+          <div className={classes.rs_school}>
+            <a href="https://rs.school/js/">
+              <img src="../../src/assets/svg/rs_school_js.svg" alt="RS_School_logo" width="100%"></img>
+            </a>
+          </div>
+        </div>
+        <div className={classes.page_up}>
+          <a href="#">
+            <img src="../../src/assets/svg/arrow_up.svg" alt="page_up" width="100%"></img>
+          </a>
+        </div>
       </Container>
     </footer>
   );
