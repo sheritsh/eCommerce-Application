5d1d50da5861de76c296e1a2b8f7936c9ae6fbe9
sheritsh <sheritsh@student.21-school.ru>
2023-08-22 00:54:38 +0300

    fix: error handle message street validation

--- src/components/UI/forms/form/type.ts
+++ src/components/UI/forms/form/type.ts
@@ -21,8 +21,8 @@ export const enum ErrorMessages {
   NotValidFirstName = 'First name: must contain at least one character and not contain special characters or numbers',
   NotValidLastName = 'Last name: must contain at least one character and not contain special characters or numbers.',
   NotValidBirthDay = 'You must be over 13 years old',
-  NotValidStreet = 'Street: must contain at least one character and ',
-  NotValidCity = 'City: must contain at least 1 character, should not include special characters, and must not start or end with a space.',
+  NotValidStreet = 'Street: must contain at least 1 character, should not include special characters, and must not start or end with a space.',
+  NotValidCity = 'City: must contain at least one character',
   NotValidPostCode = 'The index of your country must contain 5 digits',
 }
 
