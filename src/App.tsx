7dd13f4ad88da479ffd28704ae7fa6057007820d
ekatrif <katiapervak@gmail.com>
2023-08-28 12:49:25 +0300

    feat: add lasy loading of components & suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,64 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
-import ProfilePage from './pages/ProfilePage';
-import CartPage from './pages/CartPage';
-import Footer from './components/Footer/Footer';
-import NotFoundPage from './pages/NotFoundPage';
 import PrivateRoute from './components/Routing/PrivateRoute';
 import GuestRoute from './components/Routing/GuestRoute';
-import CatalogPage from './pages/CatalogPage';
-import CategoryPage from './pages/CategoryPage';
+
+const Header = lazy(() => import('./components/Header/Header'));
+const MainPage = lazy(() => import('./pages/MainPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
+const CartPage = lazy(() => import('./pages/CartPage'));
+const Footer = lazy(() => import('./components/Footer/Footer'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage'));
+const CategoryPage = lazy(() => import('./pages/CategoryPage'));
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
+      <Suspense fallback={<div className="container">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/categories/:categoryId" element={<CategoryPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+          <Route
+            path="/login"
+            element={
+              <GuestRoute>
+                <LoginPage />
+              </GuestRoute>
+            }
+          />
+          <Route
+            path="/registration"
+            element={
+              <GuestRoute>
+                <RegistrationPage />
+              </GuestRoute>
+            }
+          />
 
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/categories/:categoryId" element={<CategoryPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route
-          path="/login"
-          element={
-            <GuestRoute>
-              <LoginPage />
-            </GuestRoute>
-          }
-        />
-        <Route
-          path="/registration"
-          element={
-            <GuestRoute>
-              <RegistrationPage />
-            </GuestRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <ProfilePage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/shopping-cart"
-          element={
-            <PrivateRoute>
-              <CartPage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/shopping-cart"
+            element={
+              <PrivateRoute>
+                <CartPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
