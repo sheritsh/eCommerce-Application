8ffe8c12de5bb454144152c7b385a06a48587a7b
sheritsh <sheritsh@student.21-school.ru>
2023-09-04 09:15:36 +0300

    feat: add search form

---
+++ src/features/filters/search/SearchForm.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+
+const SearchForm: React.FC<{ onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void }> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    setTimeout(onSearch({ target: { value: searchQuery } } as React.ChangeEvent<HTMLInputElement>), 1000);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField type="search" placeholder="Search products" value={searchQuery} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+export default SearchForm;
