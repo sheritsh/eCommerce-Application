064cfa0fc46ecc6ba3b6ce56715569c36771a5a9
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-09-04 04:33:35 +0300

    feat: edit personal info popup

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -7,14 +7,13 @@ import classes from './Profile.module.scss';
 import { fetchCustomer } from './customer-slice';
 import { IRootState } from '../types';
 import Button from '../../components/UI/button/Button';
+import EditPersonalInfo from './EditPersonalInfo/EditPersonalInfo';
 
 const Profile: React.FC = () => {
   const dispatch = useAppDispatch();
   const customer = useSelector((state: IRootState) => state.customer.customerData).result;
   const token = useSelector((state: IRootState) => state.auth.authData.accessToken);
   const { addresses } = customer;
-  // eslint-disable-next-line no-console
-  console.log(customer, addresses);
 
   useEffect(() => {
     dispatch(fetchCustomer(token));
@@ -24,22 +23,26 @@ const Profile: React.FC = () => {
     <div className={classes.profile}>
       <div className={classes.profile__title}>
         <h3>Personal information</h3>
-        <Button text="edit" />
+        <EditPersonalInfo />
       </div>
       <PersonalInfo firstName={customer.firstName} lastName={customer.lastName} birthDate={customer.dateOfBirth} />
       <div className={classes.profile__title}>
         <h3>Addresses</h3>
-        <Button text="edit" />
+        <Button text="Add new address" />
       </div>
       {addresses.map((address) => (
-        <Addresses
-          id={address.id}
-          country={address.country}
-          city={address.city}
-          street={address.streetName}
-          postCode={address.postalCode}
-          customer={customer}
-        />
+        <div>
+          <Addresses
+            id={address.id}
+            country={address.country}
+            city={address.city}
+            street={address.streetName}
+            postCode={address.postalCode}
+            customer={customer}
+          />
+          <button>edit</button>
+          <button>delete</button>
+        </div>
       ))}
       <span></span>
     </div>
