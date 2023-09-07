4b40163853eb6fcf5d12d19b8ebbcbc10d306fe3
sheritsh <sheritsh@student.21-school.ru>
2023-09-07 07:32:09 +0300

    fix: get product by id path

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,6 +3,7 @@ import { Settings, Languages } from './types';
 
 const Endpoints = {
   AUTH: 'https://auth.us-central1.gcp.commercetools.com',
+  GET_DETAILED_PRODUCT: `${ENV.Host}/${ENV.ProjectKey}/products`,
   GET_PRODUCTS: `${ENV.Host}/${ENV.ProjectKey}/product-projections`,
   GET_PRODUCTS_BY_CATEGORY: `${ENV.Host}/${ENV.ProjectKey}/product-projections/search?staged=true&filter=categories.id:`,
   GET_SEARCH: `${ENV.Host}/${ENV.ProjectKey}/product-projections/search?staged=true&limit=${Settings.ProductsPerPage}&text.${Languages.English}=`,
