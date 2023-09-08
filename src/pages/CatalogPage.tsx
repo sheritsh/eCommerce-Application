18acdee4667b89606ce42beca2eff3900c6e87d0
ekatrif <katiapervak@gmail.com>
2023-09-08 16:10:43 +0300

    refactor: move sorting logic to SortForm component

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
+import React from 'react';
 import SearchForm from '../features/filters/Search/SearchForm';
 import Container from '../components/UI/container/Container';
 import ProductsByParams from '../features/filters/ProductsByParams/ProductsByParams';
 import Categories from '../features/Categories/Categories';
 import Filters from '../components/Filters/Filters';
-import { useAppDispatch } from '../store';
 import SortForm from '../features/filters/Sorting/SortForm';
-import { fetchProductsBySort } from '../features/filters/Sorting/fetch-products-by-sort';
 
 const Catalog: React.FC = () => {
-  const routerParams = useParams();
-  const { categoryId } = routerParams;
-  const dispatch = useAppDispatch();
-
-  const [sortQuery, setSortQuery] = useState('');
-
-  const handleSort = (sortOption: string): void => {
-    setSortQuery(sortOption);
-  };
-
-  useEffect(() => {
-    let endpoint = '';
-    if (sortQuery) {
-      if (sortQuery === 'sort=name.en-US') {
-        endpoint = `?${sortQuery}`;
-      } else {
-        endpoint = `search?${sortQuery}`;
-      }
-      if (endpoint) dispatch(fetchProductsBySort(endpoint, categoryId));
-    }
-  }, [sortQuery]);
-
-
-
   return (
     <div className="content">
       <Container>
@@ -47,7 +20,7 @@ const Catalog: React.FC = () => {
             <div className="catalog-main">
               <div className="catalog-nav">
                 <SearchForm />
-                <SortForm onSort={handleSort} />
+                <SortForm />
               </div>
               <ProductsByParams />
             </div>
