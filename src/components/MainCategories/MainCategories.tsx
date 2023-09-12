9984b72d67c9dfdbc5657f45072f03c7be6a1bd2
ekatrif <katiapervak@gmail.com>
2023-09-12 22:29:14 +0300

    fix: size of categories image & title

--- src/components/MainCategories/MainCategories.tsx
+++ src/components/MainCategories/MainCategories.tsx
@@ -5,6 +5,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
+import { Link } from 'react-router-dom';
 import categories from '../../assets/data/main-categories';
 import classes from './MainCategories.module.scss';
 
@@ -13,27 +14,29 @@ const MainCategories: React.FC = () => {
     <Box sx={{ width: '100%', height: 'auto', overflowY: 'none' }}>
       <ImageList variant="masonry" cols={2} gap={8}>
         {categories.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              sx={{
-                background: `linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)`,
-              }}
-              title={<div className={classes.title}>item.title</div>}
-              position="bottom"
-              actionIcon={
-                <IconButton sx={{ color: 'white' }} aria-label={`star ${item.title}`}>
-                  <StarBorderIcon />
-                </IconButton>
-              }
-              actionPosition="right"
-            />
-          </ImageListItem>
+          <Link to="#">
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                sx={{
+                  background: `linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)`,
+                }}
+                title={<div className={classes.title}>{item.title}</div>}
+                position="bottom"
+                actionIcon={
+                  <IconButton sx={{ color: 'white' }} aria-label={`star ${item.title}`}>
+                    <StarBorderIcon />
+                  </IconButton>
+                }
+                actionPosition="right"
+              />
+            </ImageListItem>
+          </Link>
         ))}
       </ImageList>
     </Box>
